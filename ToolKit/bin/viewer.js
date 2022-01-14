e970d5cfa65f8dd644c5aaec552d98cbf7fcb670
Leonardo Maron <leo22.maron@hotmail.com>
2022-01-14 16:56:46 -0300

    Viewer - created

---
+++ ToolKit/bin/viewer.js
@@ -0,0 +1,28 @@
+// ---- Imports ---- //
+
+const webSocket = require('ws');
+
+
+// ---- Startup / Main ---- //
+
+const args = process.argv.slice(2);
+
+if (!args[0]) {
+  console.error('You need to write the IP of your computer (transmitter).');
+  console.log('viewer <IP>');
+  console.log('The IP need a prefix and postfix: ws://<IP>:3333');
+
+  process.exit();
+}
+
+const ws = new webSocket(args[0]);
+
+ws.on('message', (msg) => {  // Write the broadcast in console
+  console.clear();
+
+  const sprite = JSON.parse(msg);
+
+  sprite.forEach((line) => {
+    console.log(line);
+  });
+});
