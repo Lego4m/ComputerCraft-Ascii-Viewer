db84549e40e8ee91682f46139075b7d144a5f128
Leonardo Maron <leo22.maron@hotmail.com>
2022-01-14 20:01:04 -0300

    Viewer Minecraft - created

---
+++ Minecraft/viewer.lua
@@ -0,0 +1,53 @@
+-- Imports
+
+os.loadAPI("json");
+
+
+-- Variables
+
+local args = {...};
+local monitor = peripheral.find("monitor");
+
+
+-- Functions
+
+function drawSprite(sprite)
+  for i=1,#sprite do
+    monitor.setCursorPos(1, i);
+    monitor.write(sprite[i]);
+  end
+end
+
+
+-- Startup
+
+if not (args[1]) then
+  print("You need to write the IP of your computer (transmitter).");
+  print("viewer <IP> <textScale (optional | 0.5 - 5)>");
+  print("The IP need a prefix and postfix: ws://<IP>:3333");
+
+  error();
+end
+
+if (args[2]) then
+  monitor.setTextScale(tonumber(args[2]));
+end
+
+monitor.clear();
+
+
+-- Main
+
+local ws, err = http.websocket(args[1]);
+
+if (ws) then
+  print("Connected.");
+
+  while (true) do
+    local msg = ws.receive();
+    local sprite = json.decode(msg);
+    drawSprite(sprite);
+  end
+elseif (err) then
+  print(err);
+end
